ff35027e3ffbd9a5572b0383e0ba5e052ab9f848
Taurese <43673192+25487@users.noreply.github.com>
2020-01-21 17:03:41 +0100

    Add files via upload

---
+++ opdracht.php
@@ -0,0 +1,15 @@
+
+<?php
+
+//opdracht 1
+use PHPUnit\Framework\TestCase;
+
+class addContact extends TestCase
+{
+    public function addContacts()
+    $addContacts -> addNewContact;
+    $addContacts -> addNewTelefoonNummer;
+    $addContacts -> CheckTelefoonNummer;
+    }
+}
+?>
